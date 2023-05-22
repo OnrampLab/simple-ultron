657edbb7381a989199a80cc35b140327fc69f505
Kos Huang <koshuang@gmail.com>
2023-05-22 17:39:22 +0800

    feat: add prompt form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
+import { Col, Row } from 'antd';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PromptForm from './PromptForm';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row gutter={16}>
+      <Col className="gutter-row" span={24}>
+        <PromptForm />
+      </Col>
+    </Row>
   );
-}
+};
 
 export default App;
