0e0bf0fe002cd06518083bd3e0e17a58c2ebbcf2
Kos Huang <koshuang@gmail.com>
2023-05-25 16:54:04 +0800

    chore: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { Col, Row } from 'antd';
 import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { PromptManager } from './infrastructure/ui/components/PromptManager';
 import { GlobalModalProvider } from './infrastructure/ui/providers/GlobalModalProvider';
 
-const App: React.FC = () => {
-  return (
-    <GlobalModalProvider>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
       <Row gutter={16}>
         <Col className="gutter-row" span={24}>
           <PromptManager />
         </Col>
       </Row>
+    ),
+  },
+]);
+
+const App: React.FC = () => {
+  return (
+    <GlobalModalProvider>
+      <RouterProvider router={router} />
     </GlobalModalProvider>
   );
 };
